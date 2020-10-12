2829c8cfaeee770a5f31becf81dc2104175a89ab
Snahier <hansalmeida@hotmail.com>
2020-10-12 10:10:27 -0300

    Add orphanages page layout

--- web/src/pages/OrphanagesMap.tsx
+++ web/src/pages/OrphanagesMap.tsx
@@ -1,12 +1,43 @@
 import React from "react"
+import {
+  Aside,
+  City,
+  CreateOrphanage,
+  Description,
+  Footer,
+  Header,
+  MapMarkerImg,
+  OrphanagesMapContainer,
+  State,
+  Title,
+} from "../styles/pages/orphanagesMap"
+import { FiPlus } from "react-icons/fi"
 
 interface OrphanagesMapProps {}
 
 const OrphanagesMap: React.FC<OrphanagesMapProps> = () => {
   return (
-    <div>
-      <h1>OrphanagesMap Component</h1>
-    </div>
+    <OrphanagesMapContainer>
+      <Aside>
+        <Header>
+          <MapMarkerImg />
+
+          <Title>Escolha um orfanato no mapa</Title>
+          <Description>
+            Visite orfanatos e mude o dia de muitas crianças.
+          </Description>
+        </Header>
+
+        <Footer>
+          <City>Salvador</City>
+          <State>Bahia</State>
+        </Footer>
+      </Aside>
+
+      <CreateOrphanage to="">
+        <FiPlus size={32} color="#fff" />
+      </CreateOrphanage>
+    </OrphanagesMapContainer>
   )
 }
 export default OrphanagesMap
---
+++ web/src/styles/pages/orphanagesMap.ts
@@ -0,0 +1,73 @@
+import { Link } from "react-router-dom"
+import styled from "styled-components"
+
+import mapMarkerImg from "../../Assets/map-marker.svg"
+
+export const OrphanagesMapContainer = styled.div`
+  position: relative;
+
+  display: flex;
+
+  width: 100vw;
+  height: 100vh;
+`
+
+export const Aside = styled.aside`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+
+  width: 440px;
+  padding: 80px;
+
+  background: linear-gradient(329.54deg, #29b6d1 0%, #00c7c7 100%);
+`
+export const Header = styled.header``
+export const MapMarkerImg = styled.img.attrs({
+  src: mapMarkerImg,
+  alt: "Happy",
+})``
+export const Title = styled.h2`
+  margin-top: 64px;
+
+  font-size: 40px;
+  font-weight: 800;
+  line-height: 42px;
+`
+export const Description = styled.p`
+  margin-top: 24px;
+
+  line-height: 28px;
+`
+export const Footer = styled.footer`
+  display: flex;
+  flex-direction: column;
+
+  line-height: 24px;
+`
+export const City = styled.strong`
+  font-weight: 800;
+`
+export const State = styled.span``
+
+export const CreateOrphanage = styled(Link)`
+  position: absolute;
+  right: 40px;
+  bottom: 40px;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  width: 64px;
+  height: 64px;
+
+  background: #14c3d6;
+  border-radius: 20px;
+
+  transition: background-color 0.2s;
+
+  &:hover {
+    background: #17d6eb;
+  }
+`
