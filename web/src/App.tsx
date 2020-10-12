eb6ad127869010375430309568f0b56965cc73c4
Snahier <hansalmeida@hotmail.com>
2020-10-12 09:05:07 -0300

    Add global styles and default css

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,9 +1,11 @@
 import React from "react"
+import { GlobalStyles } from "./styles/GlobalStyles"
 
 function App() {
   return (
     <div className="App">
       <h1>Next Level Week #3 - Happy</h1>
+      <GlobalStyles />
     </div>
   )
 }
