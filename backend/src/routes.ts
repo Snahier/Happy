4f517a6855926809ae7cd952c835f52b19d87a71
Snahier <hansalmeida@hotmail.com>
2020-10-13 09:26:31 -0300

    Create find one orphanage by ID request

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -4,6 +4,7 @@ import OrphanagesController from "./controllers/OrphanagesController"
 const routes = Router()
 
 routes.get("/orphanages", OrphanagesController.getAll)
+routes.get("/orphanages/:id", OrphanagesController.findOne)
 routes.post("/orphanages", OrphanagesController.create)
 
 export default routes
