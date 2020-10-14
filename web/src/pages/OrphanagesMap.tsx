7dbc0f54d60644fa9618764d7745602f7a49aae1
Snahier <hansalmeida@hotmail.com>
2020-10-14 09:15:57 -0300

    Add Axios, List orphanages on map

--- web/src/pages/OrphanagesMap.tsx
+++ web/src/pages/OrphanagesMap.tsx
@@ -1,4 +1,4 @@
-import React from "react"
+import React, { useEffect, useState } from "react"
 import {
   Aside,
   City,
@@ -19,10 +19,26 @@ import { FiPlus, FiArrowRight } from "react-icons/fi"
 import { Map, TileLayer, Marker } from "react-leaflet"
 
 import { mapIcon } from "../utils/mapIcon"
+import api from "../services/api"
 
 interface OrphanagesMapProps {}
 
+interface Orphanage {
+  id: number
+  name: string
+  latitude: number
+  longitude: number
+}
+
 const OrphanagesMap: React.FC<OrphanagesMapProps> = () => {
+  const [orphanages, setOrphanages] = useState<Orphanage[]>([])
+
+  useEffect(() => {
+    api.get("orphanages").then(({ data }) => {
+      setOrphanages(data)
+    })
+  }, [])
+
   return (
     <OrphanagesMapContainer>
       <Aside>
@@ -53,14 +69,16 @@ const OrphanagesMap: React.FC<OrphanagesMapProps> = () => {
           url={`https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/256/{z}/{x}/{y}@2x?access_token=${process.env.REACT_APP_MAPBOX_TOKEN}`}
         />
 
-        <Marker position={[-12.9950098, -38.4681732]} icon={mapIcon}>
-          <Popup closeButton={false} minWidth={240} maxWidth={240}>
-            Lar das meninas
-            <PopupLink to="/orphanages/1">
-              <FiArrowRight size={20} color="#fff" />
-            </PopupLink>
-          </Popup>
-        </Marker>
+        {orphanages.map(({ id, latitude, longitude, name }) => (
+          <Marker key={id} position={[latitude, longitude]} icon={mapIcon}>
+            <Popup closeButton={false} minWidth={240} maxWidth={240}>
+              {name}
+              <PopupLink to={`/orphanages/${id}`}>
+                <FiArrowRight size={20} color="#fff" />
+              </PopupLink>
+            </Popup>
+          </Marker>
+        ))}
       </Map>
 
       <CreateOrphanage to="/orphanages/create">
